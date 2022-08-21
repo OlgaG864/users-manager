2a9c7a3d4efb6a7bbdad93ea106fc8b619a28179
levworkshop <noreply@githubusers.com>
2022-08-21 20:22:18 +0300

    add state in components

--- client/src/components/Users/Users.tsx
+++ client/src/components/Users/Users.tsx
@@ -11,20 +11,32 @@ export interface IUser {
     _id?: string;
 }
 
-interface UsersProps {
-}
-
 interface UsersState {
-
+    users: Array<IUser>;
 }
 
-class Users extends React.Component<UsersProps, UsersState> {
+class Users extends React.Component<{}, UsersState> {
+
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            users: []
+        }
+    }
 
     render() {
         return (
             <div className="bg-dark bg-opacity-10 border px-2">
                 <Header />
-                <Table users={[]} />
+                {
+                    (this.state.users.length === 0) &&
+                    <div className="alert alert-warning" role="alert">
+                        No users to display.
+                    </div>
+                }
+
+                <Table users={this.state.users} />
             </div>
         );
     }
