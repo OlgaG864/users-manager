158891dc53d374ca89969e86824b508aac864c86
olga <goncherenkoola@gmail.com>
2022-08-24 15:46:50 +0300

    users app

---
+++ src/Table/Table.tsx
@@ -0,0 +1,52 @@
+import Status from "../Status/Status";
+import { IUser } from "../Users/Users";
+
+interface Props {
+  users: Array<IUser>;
+}
+
+function Table(props: Props) {
+  return (
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <th className="w-25">Full Name</th>
+          <th className="w-25">Status</th>
+          <th className="w-50">Email</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* <tr className="bg-light">
+                    <td>AAA</td>
+                    <td>
+                        <Status />
+                    </td>
+                    <td>uuu@gamil.com</td>
+                    <td>
+                        <button className="btn btn-default">
+                            <i className="bi-trash3"></i>
+                        </button>
+                    </td>
+                </tr> */}
+
+        {props.users.map((user) => (
+          <tr key={user._id} className="bg-light">
+            <td>{user.fullName}</td>
+            <td>
+              <Status type={user.status} />
+            </td>
+            <td>{user.email}</td>
+            <td>
+              <button className="btn btn-default">
+                <i className="bi-trash3"></i>
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
