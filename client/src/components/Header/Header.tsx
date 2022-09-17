3947a6373a91a35058e32bb623944704ac51441b
olga <goncherenkoola@gmail.com>
2022-09-17 14:34:54 +0300

    changes

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,50 +1,81 @@
 import React from "react";
+import { StatusType } from "../Users/Users";
 
 interface HeaderProps {
-
+  addUser: Function;
 }
 
 interface HeaderState {
-    fullName: string;
-    email: string;
+  fullName: string;
+  email: string;
+  status: StatusType;
 }
 
 class Header extends React.Component<HeaderProps, HeaderState> {
+  constructor(props: HeaderProps) {
+    super(props);
 
-    constructor(props: HeaderProps) {
-        super(props);
+    this.state = {
+      fullName: "",
+      email: "",
+      status: "active",
+    };
+  }
 
-        this.state = {
-            fullName: '',
-            email: ''
-        }
-    }
+  handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    fieldName: string
+  ) => {
+    this.setState({
+      ...this.state,
+      [fieldName]: event.target.value,
+    });
+  };
+  addUser = () => {
+    this.props.addUser({
+      fullName: this.state.fullName,
+      email: this.state.email,
+      status: this.state.status,
+    });
 
-    handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, fieldName: string) => {
-        this.setState({
-            ...this.state,
-            [fieldName]: event.target.value
-        })
-    }
+    this.setState(() => ({
+      fullName: "",
+      email: "",
+    }));
+  };
 
-    render() {
-        return (
-            <div className="d-flex align-items-center p-3 my-4 bg-light" >
-                <h5 className="me-auto mb-0">Users</h5>
-                <div className="d-flex">
-                    <input value={this.state.fullName}
-                        onChange={(e) => this.handleInputChange(e, 'fullName')}
-                        type="text" placeholder="Name" className="form-control" />
+  render() {
+    return (
+      <div className="d-flex align-items-center p-3 my-4 bg-light">
+        <h5 className="me-auto mb-0">Users</h5>
+        <div className="d-flex  align-content-sm-between">
+          <input
+            value={this.state.fullName}
+            onChange={(e) => this.handleInputChange(e, "fullName")}
+            type="text"
+            placeholder=" Full Name"
+            className="form-control"
+          />
 
-                    <input value={this.state.email}
-                        onChange={(e) => this.handleInputChange(e, 'email')}
-                        type="text" placeholder="Email" className="form-control mx-3" />
+          <input
+            value={this.state.email}
+            onChange={(e) => this.handleInputChange(e, "email")}
+            type="text"
+            placeholder="Email"
+            className="form-control mx-3"
+          />
 
-                    <button className="btn btn-info text-white">Add</button>
-                </div>
-            </div>
-        );
-    }
+          <select className="form-control " name="status">
+            <option>{this.state.status}</option>
+          </select>
+
+          <button onClick={this.addUser} className="btn btn-info text-white ">
+            Add
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
