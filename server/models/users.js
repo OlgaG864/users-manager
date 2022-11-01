cbecdf23d242ca5ec5f31e35ea422e9f5b68a5bc
olga <goncherenkoola@gmail.com>
2022-11-01 12:13:14 +0200

    select

--- server/models/users.js
+++ server/models/users.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    fullName: {
-        type: String,
-        require: true,
-    },
-    status: {
-        type: String,
-        require: true,
-    },
-    email: {
-        type: String,
-        require: true,
-    },
+  fullName: {
+    type: String,
+    require: true,
+  },
+  status: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 exports.User = User;
